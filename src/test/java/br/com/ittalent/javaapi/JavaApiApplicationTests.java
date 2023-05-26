48e0dd01fbf62f60ba5b4eae4b8102b932e8a182
kelvin.alef <kelvin.alefs@gmail.com>
2023-05-26 14:03:42 -0300

    sprint initializr

---
+++ src/test/java/br/com/ittalent/javaapi/JavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ittalent.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
